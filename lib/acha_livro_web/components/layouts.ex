4a37066aaade6606bc9a723c9f3ed055981874fd
otaviouu <otavio.venancio.oliveira@usp.br>
2025-08-10 21:25:11 -0300

    feat: add theme toggle component to the navigation bar

--- lib/acha_livro_web/components/layouts.ex
+++ lib/acha_livro_web/components/layouts.ex
@@ -44,6 +44,9 @@ defmodule AchaLivroWeb.Layouts do
       </div>
       <div class="flex-none">
         <ul class="flex flex-column px-1 space-x-4 items-center">
+          <li>
+            <.theme_toggle />
+          </li>
           <%= if @current_scope do %>
             <li>
               {@current_scope.user.email}
